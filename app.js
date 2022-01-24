206f5c90746d6f104e2278133587bd97fb819a12
icorvus <business.wrona@gmail.com>
2022-01-24 19:08:52 +0100

    Added functions for basic arithmetics

---
+++ app.js
@@ -0,0 +1,16 @@
+function add(numberA, numberB) {
+  return numberA + numberB;
+}
+
+function substract(numberA, numberB) {
+  return numberA - numberB;
+}
+
+function multiply(numberA, numberB) {
+  return numberA * numberB;
+}
+
+function divide(numberA, numberB) {
+  return numberA / numberB;
+}
+
