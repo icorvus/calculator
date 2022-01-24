66dd6f5739b65b5837e104e3b0967ffea412dd00
icorvus <business.wrona@gmail.com>
2022-01-24 19:21:31 +0100

    Added operate function

--- app.js
+++ app.js
@@ -14,3 +14,21 @@ function divide(numberA, numberB) {
   return numberA / numberB;
 }
 
+function operate(operator, numberA, numberB) {
+  switch(operator) {
+    case '+':
+      return add(numberA, numberB);
+      break;
+    case '-':
+      return substract(numberA, numberB);
+      break;
+    case '*':
+      return multiply(numberA, numberB);
+      break;
+    case '/':
+      return divide(numberA, numberB);
+      break;
+    default:
+      alert(`${operator} is not yet available`)
+  }
+}
